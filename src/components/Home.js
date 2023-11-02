9251677eb8be0d3233a644b6c1873fb1078c9d17
Elijah Nyasiando <143831019+mgkEdits@users.noreply.github.com>
2023-11-02 21:31:54 +0300

    completed lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
+import { outlet} from "react-router-dom";
 
 function Home() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+  <h1>Home Page</h1>
+
+  </div>;
 }
 
 export default Home;
